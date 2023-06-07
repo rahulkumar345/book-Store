5f84769dc72eef7490325dcfc753bc6e01c3dcf8
rahul-legalpay <rahul.kumar@legalpay.in>
2023-06-07 12:44:33 +0530

    changes

--- backend/routes/books.routes.js
+++ backend/routes/books.routes.js
@@ -1,16 +1,5 @@
 const router = require("express").Router();
-// const { getCurrencies } = require("../controllers/currencies.controller");
-const { createNewBlog } = require("../controllers/books.controllers");
-const { deleteBlogs } = require("../controllers/books.controllers");
 const { getAllBooks } = require("../controllers/books.controllers");
-const {
-  searchBlogs,
-  updateBlogsWithId,
-} = require("../controllers/books.controllers");
-
-router.post("/new", createNewBlog);
 router.get("/", getAllBooks);
-router.get("/search", searchBlogs);
-router.delete("/:id", deleteBlogs);
-router.patch("/:id", updateBlogsWithId);
+
 module.exports = router;
--- backend/services/books.services.js
+++ backend/services/books.services.js
@@ -1,13 +1,5 @@
 const Blogs = require("../models/books.models");
-const findAllBlogs = async () => {
-  const result = await Blogs.find({});
-  return result;
-};
-const createBlogs = async (e) => {
-  const result = new Blogs(e);
-  const res = await result.save();
-  return res;
-};
+
 class BlogService {
   save = async (doc) => {
     const res = await doc.save();
